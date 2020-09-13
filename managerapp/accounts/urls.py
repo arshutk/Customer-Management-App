9e74af7c551dea41f510975e943b9e305c002614
Utkarsh Patel <karshxp@gmail.com>
2020-09-13 18:49:37 +0530

    Completed CRUD

--- managerapp/accounts/urls.py
+++ managerapp/accounts/urls.py
@@ -5,6 +5,10 @@ from . import views # import view will not work & from .views import home as acc
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('customer/', views.customer, name='customer'),
+    path('customer/<int:pk>', views.customer, name='customer'),
+    
+    path('create_order/<int:pk>/', views.createOrder, name='create_order'),
+    path('update_order/<int:pk>/', views.updateOrder, name='update_order'),
+    path('delete_order/<int:pk>/', views.deleteOrder, name='delete_order'),
 
 ]
--- managerapp/accounts/views.py
+++ managerapp/accounts/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 from .models import *
 
+from .forms import OrderForm
+from django.forms import inlineformset_factory
+
+
+# For templates: accounts->templates->accounts->then searches for called HTML file
+
 def home(request):
     orders = Order.objects.all()
     customers  = Customer.objects.all()
     
-    total_customers = customers.count()
+    # total_customers = customers.count()
     total_orders = orders.count()
     delivered = orders.filter(status='Delivered').count()
     pending = orders.filter(status='Pending').count()
@@ -25,7 +31,49 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
 
-# For templates: accounts->templates->accounts->then searches for called HTML file
+    orders = customer.order_set.all()
+    total_orders = orders.count # Cannot directly call function insite {{}} inside template
+
+    context = {'customer':customer,
+               'orders':orders,
+               'total_orders': total_orders, 
+              }
+    return render(request, 'accounts/customer.html', context)
+
+def createOrder(request, pk):
+    customer = Customer.objects.get(id=pk)
+    form = OrderForm(initial={'customer': customer})
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    context = {'form':form}
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance= order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {'item': order}
+    return render(request, 'accounts/delete.html', context)
